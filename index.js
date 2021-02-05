a08d132981650fc6c0a42134ef981e5f7f385a6b
Colin Mudie <csmudie1@gmail.com>
2021-02-05 14:41:59 -0500

    added inquirer to index

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Title of Project? ', 'Brief Description: '];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: "title",
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: "description",
+            message: questions[1]
+        }
+    ]).then((data) => {
+        console.log(data);
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+writeToFile();
