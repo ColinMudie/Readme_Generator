87ea1e74b0945332e2fd36b7520ed9ff9cff918b
Colin Mudie <csmudie1@gmail.com>
2021-02-05 16:22:09 -0500

    added generateMarkdown to project, can now write input title to new file

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = ['Title of Project? ', 'Brief Description: '];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: "title",
-            message: questions[0]
-        },
-        {
-            type: 'input',
-            name: "description",
-            message: questions[1]
-        }
-    ]).then((data) => {
-        console.log(data);
-    })
+    console.log(fileName);
+    console.log(data);
+    fs.writeFile(`${data.title}.md`, generateMarkdown(data), (err) => 
+    err ? console.log(err) : console.log('it works'));
 }
+inquirer.prompt([
+    {
+        type: 'input',
+        name: "title",
+        message: questions[0]
+    },
+    {
+        type: 'input',
+        name: "description",
+        message: questions[1]
+    }
+]).then((data) => {
+    writeToFile(data.title, data);
+});
+
+
+
+
+
 
 // TODO: Create a function to initialize app
-// function init() {}
+// function init() {
+    
+// }
 
-// // Function call to initialize app
+// // // Function call to initialize app
 // init();
-writeToFile();
+
